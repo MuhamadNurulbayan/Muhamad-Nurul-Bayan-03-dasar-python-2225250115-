89f7433115579120c4a2711d95c8011064ac8c2a
MuhamadNurulbayan <nurulbayan22@gmail.com>
2026-09-18 06:57:22 +0700

    Add comparison script for two numbers

---
+++ Latihan/02_bandingkan_dua_bilangan.py
@@ -0,0 +1,10 @@
+a = float(input("Masukkan bilangan pertama: "))
+b = float(input("Masukkan bilangan kedua: "))
+
+if a > b:
+    print("Bilangan pertama lebih besar")
+else:
+    if a < b:
+        print("Bilangan kedua lebih besar")
+    else:
+        print("Kedua bilangan sama")
